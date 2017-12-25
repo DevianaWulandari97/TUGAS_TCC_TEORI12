98ca4c3180a9b86057b23d98cfd9bfb214188946
DevianaWulandari97 <33819263+DevianaWulandari97@users.noreply.github.com>
2017-12-25 21:49:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<b>";
+echo "<center><h1>Tugas Teori TCC 12 - Docker Compose</h1></center>";
+echo "<br>";
+echo "<h3>Nama 		: DEVIANA WULANDARI<br>";
+echo "NIM 		    : 155410082<br>";
+echo "Jurusan 		: Teknik Informatika<br>";
+echo "Github		: github.com/DevianaWulandari97</h3>";
+echo "</b>";
+?>
